905d296103feec1ca9f95ed522f0a8f404be3d7c
NuhoZubaedSalam <zs.nuho@gmail.com>
2025-08-10 11:58:43 +0200

    form reset after redirection

--- js/login.js
+++ js/login.js
@@ -1,12 +1,19 @@
 function validateInput(event) {
+    // Prevents the form from resetting when when the submit button is clicked
     event.preventDefault(); 
     
+    // Gets the user's email and password from the input fields
     const email = document.getElementById("user-email").value;
     const password = document.getElementById("user-password").value;
 
+    // Redirects to the banking page if the credentials are correct
+    // Otherwise it shows an error message
     if (email === "admin@domain.com" && password === "banking")
-        window.location.href = "./banking.html";
+        window.location.href = "../banking.html";
     else
         return alert("Wrong credentials");
-}
 
+    // Resets the login form
+    document.getElementById("login-form").reset();
+
+}
