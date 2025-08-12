5d2d865678f28a54cf29123ae76948e256f2f7de
NuhoZubaedSalam <zs.nuho@gmail.com>
2025-08-12 19:57:59 +0200

    completed website

--- js/login.js
+++ js/login.js
@@ -1,18 +1,13 @@
 document.getElementById("login-form").addEventListener("submit", (event) => {
-    // Prevents the form from resetting when when the submit button is clicked
     event.preventDefault(); 
     
-    // Gets the user's email and password from the input fields
     const email = document.getElementById("user-email").value;
     const password = document.getElementById("user-password").value;
 
-    // Redirects to the banking page if the credentials are correct
-    // Otherwise it shows an error message
     if (email === "admin@domain.com" && password === "banking")
         window.location.href = "./banking.html";
     else
         return alert("Wrong credentials");
 
-    // Resets the form to its original state
     document.getElementById('login-form').reset();
 })
