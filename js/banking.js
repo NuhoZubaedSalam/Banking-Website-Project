c6a3bad9a23f9911ae3eeee10aba129e703c7cb8
NuhoZubaedSalam <zs.nuho@gmail.com>
2025-08-20 01:26:36 +0200

    completed

--- js/banking.js
+++ js/banking.js
@@ -2,7 +2,7 @@ document.getElementById("deposit-button").addEventListener("click", () => {
     const depositedAmount = parseFloat(document.getElementById("deposit-input").value);
     const currentBalance = parseFloat(document.getElementById("display-balance").innerText);
     
-    if (depositedAmount <= 0) {
+    if (isNaN(depositedAmount) || depositedAmount <= 0) {
         document.getElementById("deposit-input").value = '';
         return alert("You need to deposit at least $1");
     } else {
@@ -11,14 +11,8 @@ document.getElementById("deposit-button").addEventListener("click", () => {
         const newTotalDeposit = totalDeposit + depositedAmount;
         const newBalance = currentBalance + depositedAmount;
 
-        console.log(newTotalDeposit); 
-        console.log(newBalance);
-
         document.getElementById("display-total-deposit").innerText = newTotalDeposit.toFixed(2);
         document.getElementById("display-balance").innerText = newBalance.toFixed(2);
-
-        console.log(newTotalDeposit.toFixed(2)); 
-        console.log(newBalance.toFixed(2));
 
         document.getElementById("deposit-input").value = '';
     }
@@ -28,7 +22,7 @@ document.getElementById("withdraw-button").addEventListener("click", () => {
     const withdrawnAmount = parseFloat(document.getElementById("withdraw-input").value);
     const currentBalance = parseFloat(document.getElementById("display-balance").innerText);
 
-    if (withdrawnAmount <= 0)  {
+    if (isNaN(withdrawnAmount) || withdrawnAmount <= 0)  {
         document.getElementById("withdraw-input").value = '';
         return alert("You need to withdraw at least $1");
     } else if (withdrawnAmount > currentBalance)
@@ -39,14 +33,8 @@ document.getElementById("withdraw-button").addEventListener("click", () => {
         const newTotalWithdraw = totalWithdraw + withdrawnAmount; 
         const newBalance = currentBalance - withdrawnAmount;
 
-        console.log(newTotalWithdraw); 
-        console.log(newBalance);
-
         document.getElementById("display-total-withdraw").innerText = newTotalWithdraw.toFixed(2);
         document.getElementById("display-balance").innerText = newBalance.toFixed(2);
-
-        console.log(newTotalWithdraw.toFixed(2)); 
-        console.log(newBalance.toFixed(2));
 
         document.getElementById("withdraw-input").value = '';
     }
